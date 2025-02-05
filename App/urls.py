4c78697db0f393cd7e989669dfae4b2883982de5
danielluccio <daniel130877@gmail.com>
2025-02-05 19:12:47 -0300

    Detail e Updates criadas

--- App/urls.py
+++ App/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CarsListview, CarsCreateview
+from .views import CarsListview, CarsCreateview, CarsDetailView, UpdateCarView
 
 
 
 urlpatterns = [
     path("", CarsListview.as_view(), name='cars_list'),
     path("add_car", CarsCreateview.as_view(), name='add_car'),
+    path("<int:pk>", CarsDetailView.as_view(), name='detail_car'),
+    path("update/<int:pk>", UpdateCarView.as_view(), name='update_car')
 ]
--- App/views.py
+++ App/views.py
@@ -3,6 +3,8 @@ from .models import Car, Brand
 from .forms import BrandForm, CarForm
 from django.views.generic import ListView
 from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
 
 class CarsListview(ListView):
     model = Car
@@ -23,7 +25,19 @@ class CarsCreateview(CreateView):
     model = Car
     form_class = CarForm
     template_name = 'new_car.html'
-    success_url = 'cars_list'
+    success_url = '../cars'
 
+
+
+class CarsDetailView(DetailView):
+    model = Car
+    template_name = 'detail_car.html'
+
+
+class UpdateCarView(UpdateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'update_car.html'
+    success_url = 'list_cars'
     
     
